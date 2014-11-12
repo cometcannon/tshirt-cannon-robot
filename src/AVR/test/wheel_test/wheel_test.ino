d0c542a9d4144285bbd418898a65f21250758462
Omar Hasan <omarhasan777@gmail.com>
2014-11-12 13:37:04 -0600

    Modified wheel_test.ino to recieve velocity commands from atheros

--- src/AVR/test/wheel_test/wheel_test.ino
+++ src/AVR/test/wheel_test/wheel_test.ino
@@ -7,21 +7,55 @@
 PIDController angularVelocityController(10, 0, 0, 0, 0, 0);
 Encoder encoder(3, 4, 0);
 Servo esc;
-//Wheel wheel(5, &esc, &encoder, &angularVelocityController);
+Wheel wheel(5, &esc, &encoder, &angularVelocityController);
 
 void setup()
 {
-  esc.attach(5);
-  
-  esc.writeMicroseconds(1500);
-  delay(233);
-  esc.writeMicroseconds(2000);
-  delay(233);
-  esc.writeMicroseconds(1000);
-  delay(233);
-  esc.writeMicroseconds(1500);
-  
-  esc.writeMicroseconds(1800);
+   // esc.attach(6);
+
+   // esc.writeMicroseconds(1500);
+   // delay(233);
+   // esc.writeMicroseconds(2000);
+   // delay(233);
+   // esc.writeMicroseconds(1000);
+   // delay(233);
+   // esc.writeMicroseconds(1500);
+   // delay(2000);
+   // esc.writeMicroseconds(1500);
+
+    calibrate();
+    Serial.begin(115200);
 }
 
-void loop(){}
+void loop()
+{
+    getNextCommand();
+    delay(10);
+}
+
+void calibrate()
+{
+
+    wheel.SetThrottle(1500);
+    delay(233);
+    wheel.SetThrottle(2000);
+    delay(233);
+    wheel.SetThrottle(1000);
+    delay(233);
+    wheel.SetThrottle(1500);
+    delay(2000);
+}
+
+void getNextCommand()
+{
+    int command;
+    if(Serial.available())
+    {
+        byte _buffer[2];
+        Serial.readBytes(_buffer, 2);
+        command = _buffer[0] + (_buffer[1] << 8);
+        
+        wheel.SetThrottle(command);    
+    }
+}
+
