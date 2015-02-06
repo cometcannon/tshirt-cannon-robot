6f97fcd7cd04233bf7939134fdc8b3cd49607073
Ryan J. Marcotte <ryan@ryanjmarcotte.com>
2015-02-06 10:19:41 -0600

    add magic bytes support

--- src/AVR/test/wheel_test/wheel_test.ino
+++ src/AVR/test/wheel_test/wheel_test.ino
@@ -4,6 +4,8 @@
 #include "encoder.h"
 #include "wheel.h"
 
+#define MAGIC 0x47414e53
+
 //PIDController angularVelocityController(10, 0, 0, 0, 0, 0);
 //Encoder encoder(3, 4, 0);
 Servo esc1;
@@ -22,137 +24,83 @@ void setup()
     esc2.attach(5);
     esc3.attach(6);
     esc4.attach(9);
-
 }
 
 void loop()
 {
     getNextCommand();
-    /*
-    if(Serial1.available())
-    {
-        char c = (char)Serial1.read();
-        Serial.write(c);
-    }
-    */
     delay(10);
 }
 
-/*
-void calibrate()
+void kill_motors()
 {
-    delay(2000);
-    
-    esc1.writeMicroseconds(1500);
-    esc2.writeMicroseconds(1500);
-    esc3.writeMicroseconds(1500);
-    esc4.writeMicroseconds(1500);
-    delay(233);
-    
-    esc1.writeMicroseconds(1000);
-    esc2.writeMicroseconds(1000);
-    esc3.writeMicroseconds(1000);
-    esc4.writeMicroseconds(1000);
-    delay(233);
-    
-    esc1.writeMicroseconds(2000);
-    esc2.writeMicroseconds(2000);
-    esc3.writeMicroseconds(2000);
-    esc4.writeMicroseconds(2000);
-    delay(233);
-    
-    esc1.writeMicroseconds(1500);
-    esc2.writeMicroseconds(1500);
-    esc3.writeMicroseconds(1500);
-    esc4.writeMicroseconds(1500);
-    delay(1000);
- 
-}*/
+    float neutral = 1502.0;
 
-/*
-void calibrate()
-{
-    wheel.SetThrottle(1500);
-    delay(233);
-    wheel.SetThrottle(2000);
-    delay(233);
-    wheel.SetThrottle(1000);
-    delay(233);
-    wheel.SetThrottle(1500);
-    delay(2000);
-    
-}*/
-
-/*
-void escTest()
-{
-    esc.attach(6);
-    
-    esc.writeMicroseconds(1500);
-    delay(2000);
-    esc.writeMicroseconds(2000);
-    delay(2000);
-    esc.writeMicroseconds(1000);
-    delay(2000);
-    esc.writeMicroseconds(1500);
-    delay(2000);
-    
-    esc.writeMicroseconds(1700);
+    esc1.writeMicroseconds(neutral);
+    esc2.writeMicroseconds(neutral);
+    esc3.writeMicroseconds(neutral);
+    esc4.writeMicroseconds(neutral);
 }
-*/
+
+void cmd_single_motor(int8_t motor, int8_t value)
+{
+    float command;
+
+    if (motor == 1 || motor == 2)
+        command = -((int8_t) value) * (1000.0 / 255.0) + 1502.0;
+    else
+        command = ((int8_t) value) * (1000.0 / 255.0) + 1502.0;
+
+    switch (motor) {
+        case 0:
+            esc1.writeMicroseconds(command);
+            break;
+        case 1:
+            esc2.writeMicroseconds(command);
+            break;
+        case 2:
+            esc3.writeMicroseconds(command);
+            break;
+        case 3:
+            esc4.writeMicroseconds(command);
+            break;
+    }
+}
+
+void cmd_all_motors(int8_t value1, int8_t value2, int8_t value3, int8_t value4)
+{
+    float command1 = ((int8_t) value1) * (1000.0 / 255.0) + 1502.0;
+    float command2 = -((int8_t) value2) * (1000.0 / 255.0) + 1502.0;
+    float command3 = -((int8_t) value3) * (1000.0 / 255.0) + 1502.0;
+    float command4 = ((int8_t) value4) * (1000.0 / 255.0) + 1502.0;
+
+    esc1.writeMicroseconds(command1);
+    esc2.writeMicroseconds(command2);
+    esc3.writeMicroseconds(command3);
+    esc4.writeMicroseconds(command4);
+}
 
 void getNextCommand()
 {
-    if(Serial1.available())
-    {
+    if(Serial1.available()) {
         Serial1.readBytes(_buffer, Serial1.available());
+        
+        if (_buffer[0] == MAGIC >> 24 & 0xff &&
+            _buffer[1] == MAGIC >> 16 & 0xff &&
+            _buffer[2] == MAGIC >> 8  & 0xff &&
+            _buffer[3] == MAGIC >> 0  & 0xff   ) {
 
-        if(_buffer[0] == 1)
-        {
-          float command;
-           if(_buffer[1] == 1 || _buffer[1] == 2)
-               command = -((int8_t)(_buffer[2]))*(1000.0/255.0) + 1502.0;
-           
-           else
-               command = ((int8_t)(_buffer[2]))*(1000.0/255.0) + 1502.0;
-
-            switch(_buffer[1])
-            {
+            switch (_buffer[4]) {
                 case 0:
-                esc1.writeMicroseconds(command);
-                break;
-
+                    kill_motors();
+                    break;
                 case 1:
-                esc2.writeMicroseconds(command);
-                break;
-
+                    cmd_single_motor(_buffer[5], _buffer[6]);
+                    break;
                 case 2:
-                esc3.writeMicroseconds(command);
-                break;
-
-                case 3:
-                esc4.writeMicroseconds(command);
-                break;
-
-                default:
-                break;
+                    cmd_all_motors(_buffer[5], _buffer[6], _buffer[7], _buffer[8]);
+                    break;
             }
         }
-
-        if(_buffer[0] == 2)
-        {
-            float command1 = ((int8_t)(_buffer[1]))*(1000.0/255.0) + 1502.0;
-            float command2 = -((int8_t)(_buffer[2]))*(1000.0/255.0) + 1502.0;
-            float command3 = -((int8_t)(_buffer[3]))*(1000.0/255.0) + 1502.0;
-            float command4 = ((int8_t)(_buffer[4]))*(1000.0/255.0) + 1502.0;
-
-            esc1.writeMicroseconds(command1);
-            esc2.writeMicroseconds(command2);
-            esc3.writeMicroseconds(command3);
-            esc4.writeMicroseconds(command4);
-            
-        }
-
-
     }
 }
